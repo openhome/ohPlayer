9fa539f5ec9edff96e5ca14fefb3bfd0beda3df1
Alan Smillie <alans@emobix.co.uk>
2015-05-29 14:37:30 +0100

    Use the OpenHome dependencies mechanism.
    This commit removes the "getdeps*" scripts and relies on the OpenHome
    dependencies mechanism to get the required library and header files.
    
    Highlights
    ----------
    
    1. Remove the getdeps* scripts.
    2. Add OhNetGenerated to the list of dependencies.
       This is required to gain access to the playlist proxy header file.
    3. Re-target the Visual Studio project to VS 2010.
       This is required to ensure the required C runtime libraries match
       those of the pre-build libraries fetched from OpenHome.
    4. Update the Visual Studio library and include paths to point to the
       downloaded dependencies.
    5. Update the release build to be built against the Multi-Threaded release
       library.
    
       Fixes a link time duplicate definition error.

--- Win32/LitePipeTestApp.cpp
+++ Win32/LitePipeTestApp.cpp
@@ -2,10 +2,9 @@
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #pragma comment(lib, "comctl32.lib")
 
-#include <windows.h>
+#include <Windows.h>
 #include <shellapi.h>
 #include <commctrl.h>
-#include <strsafe.h>
 #include <process.h>
 #include <vector>
 
--- Win32/getdeps.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-rem     Copy the required libraries an include files from the OpenHome
-rem     repositories into our source tree.
-
-python -u getdeps.py
