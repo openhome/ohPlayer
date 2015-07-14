1f75cc70d3f25f69b7a49ee0559bf506012c83cc
Pete McLaughlin <pete@emobix.co.uk>
2015-07-14 09:27:28 +0300

    Fix persistent storage assumptions about buffer termination
    The persistent storage code has various assumptions about buffer termination and a couple of other issues.
    Fix them, and hex encode the values so that we can handle arbitrary data buffers correctly without any
    assumptions about their content.

--- osx/sample/ConfigPersistentStore.mm
+++ osx/sample/ConfigPersistentStore.mm
@@ -24,8 +24,9 @@ ConfigPersistentStore::~ConfigPersistentStore()
 void ConfigPersistentStore::Read(const Brx& aKey, Bwx& aDest)
 {
     AutoMutex a(iLock);
+    Brhz aKeyZ(aKey);
     
-    NSString *value = [prefs getPref: [NSString stringWithUTF8String: (const char *)aKey.Ptr()]];
+    NSString *value = [prefs getPref: [NSString stringWithUTF8String: (const char *)aKeyZ.Ptr()]];
     
     // we have a hex string representing the bytes we want to use to fill aDest.
     // convert the data to bytes in aDest
@@ -34,14 +35,16 @@ void ConfigPersistentStore::Read(const Brx& aKey, Bwx& aDest)
     if (value==nil)
         THROW(StoreKeyNotFound);
     
-    if ([value length] > (aDest.MaxBytes()+1))
+    // value is a hex encoded string with a null terminator
+    // our actual byte count will be ([value length] / 2)
+    if (([value length]/2) > (aDest.MaxBytes()))
         THROW(StoreReadBufferUndersized);
     
     const char *cvalue = [value cStringUsingEncoding:NSUTF8StringEncoding];
     if(cvalue == NULL)
         return;
     
-    for(int i=0; i<(value.length / 2); i+=2)
+    for(int i=0; i<value.length; i+=2)
     {
         int val;
         sscanf(cvalue+i, "%2x", &val);
@@ -50,16 +53,27 @@ void ConfigPersistentStore::Read(const Brx& aKey, Bwx& aDest)
     }
 }
 
+// Encode a preference buffer as a null-terminated hex string
+// This allows us to simply handle storage of arbitrary data
+// buffers using preference strings
 static char * encodePref(const Brx& aPref)
 {
-    char * buff = new char((aPref.Bytes()+1) * 2);
+    // allocate 2 bytes per source byte plus a null terminator
+    size_t encodesize = (aPref.Bytes()*2) +1;
+    char * buff = new char[encodesize];
     char * tmp = buff;
     
     if(!buff)
         return nil;
     
     for(int i=0; i < aPref.Bytes(); i++)
+    {
         sprintf(tmp, "%.2x", aPref.At(i));
+        tmp += 2;
+    }
+    
+    // Now null terminate the buffer
+    *tmp = 0;
     
     return buff;
 }
@@ -67,11 +81,15 @@ static char * encodePref(const Brx& aPref)
 void ConfigPersistentStore::Write(const Brx& aKey, const Brx& aSource)
 {
     AutoMutex a(iLock);
-    
+    Brhz aKeyZ(aKey);
+  
     // inefficient but straightforward rendering of property buffer as a prefs string
     // convert to hex representation
-    NSString *_key=[NSString stringWithUTF8String:encodePref(aKey)];
-    NSString *_value=[NSString stringWithUTF8String:encodePref(aSource)];
+    NSString *_key=[NSString stringWithUTF8String:(const char *)aKeyZ.Ptr()];
+    
+    const char *encoded = encodePref(aSource);
+    NSString *_value=[NSString stringWithUTF8String:encoded];
+    delete encoded;
     
     [prefs setPref:_key value:_value];
 }
@@ -79,8 +97,9 @@ void ConfigPersistentStore::Write(const Brx& aKey, const Brx& aSource)
 void ConfigPersistentStore::Delete(const Brx& aKey)
 {
     AutoMutex a(iLock);
+    Brhz aKeyZ(aKey);
 
-    NSString *_key = [NSString stringWithUTF8String: (const char *)aKey.Ptr()];
+    NSString *_key = [NSString stringWithUTF8String: (const char *)aKeyZ.Ptr()];
     
     [prefs setPref:_key value:nil];
 }
