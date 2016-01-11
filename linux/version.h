c9fb3ba637464fb2a1bf0bcd2dd7db97c7fcef51
Pete McLaughlin <pete@emobix.co.uk>
2016-01-11 11:21:23 +0000

    Update the player update checker URL to match new DNs entry
    The openhome.org DNS entries were recently changed, and we need to update
    this URL used to check for app updates since the config file is no longer
    on www.openhome.org

--- linux/version.h
+++ linux/version.h
@@ -8,4 +8,4 @@
 #define CURRENT_VERSION "0.0.1"
 
 // Define the location of the release manifest here.
-#define RELEASE_URL "http://www.openhome.org/releases/openhome/application.json"
+#define RELEASE_URL "http://builds.openhome.org/releases/openhome/application.json"
